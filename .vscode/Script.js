327eeaed3dc204ef24b2ccc0bb0af57710a1e75e
Pedro <pedro.rnpa@gmail.com>
2026-04-19 22:10:58 -0300

    initial commit

---
+++ .vscode/Script.js
@@ -0,0 +1,27 @@
+function toggleMode () {
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+    const img = document.querySelector("#profile img")
+
+    if(html.classList.contains('light')) {
+
+
+        img.setAttribute('src', './assets/assets/avatarwhite.png')
+    } else {
+
+    
+        img.setAttribute('src', './assets/assets/avatardark.png')
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
